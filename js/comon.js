33dbed1deb8583ba0d6c6c21079153f0b0ee2e9e
Sazedur <sazedur11ab@gmail.com>
2024-12-13 23:36:14 +0600

    Final comment

---
+++ js/comon.js
@@ -0,0 +1,153 @@
+// get the seat button
+const kbdElements = document.querySelectorAll('.kbd');
+
+// set event listners
+kbdElements.forEach((element) =>{
+    element.onclick = handleClick
+});
+
+// create a function
+function handleClick(event){
+    const ticketInnerText = event.target.innerText;
+    const ticketId = event.target.id;
+    //set the seat bg color
+    changeSeatBG(ticketId);
+    
+    //only one time clicked on each btn
+    if(event.target.classList.contains('clicked')){
+        return
+    }
+    event.target.classList.add('clicked');
+
+    if(ticketInnerText === ticketId){
+        //total seat calculation
+        getTotalSeat();
+
+        //get the table body
+        const getTableBody = document.getElementById('table-body');
+
+        //create table 
+        const tableTr = document.createElement('tr')
+
+        //create a table td
+        const setTicketId = document.createElement('td');
+        setTicketId.innerText = ticketId;
+        tableTr.appendChild(setTicketId);
+        
+        const setTicketClass = document.createElement('td');
+        setTicketClass.innerText = 'economy';
+        tableTr.appendChild(setTicketClass);
+
+        const setTicketPrice = document.createElement('td');
+        setTicketPrice.innerText = 550;
+        tableTr.appendChild(setTicketPrice);
+
+        getTableBody.appendChild(tableTr);
+
+        // get the total price section
+        const getTotalPrice = document.getElementById('total-price');
+        const totalPriceInnerText = parseFloat(getTotalPrice.innerText);
+        const price = parseFloat(setTicketPrice.innerText);
+        const totalTicketPrice = totalPriceInnerText + price;
+        getTotalPrice.innerText = totalTicketPrice;
+        
+        //get grand total 
+        const getGrandTotal = document.getElementById('grand-total');
+        const grandPrice = parseFloat(getTotalPrice.innerText);
+        getGrandTotal.innerText = grandPrice;
+        // discount area
+        if(getTableBody.childElementCount >= 4){
+            showCouponInput();
+
+            //if clicked the coupon button
+            const getCouponBtn = document.getElementById('coupon-btn');
+            getCouponBtn.addEventListener('click', function (){
+                const getInputField = document.getElementById('input-field');
+                const couponCode = getInputField.value;
+                if(couponCode === 'NEW15'){
+                    const discount = grandPrice * 0.15;
+                    const discountedPrice = grandPrice - discount;
+                    getGrandTotal.innerText = discountedPrice;
+                }
+                else if(couponCode === 'COUPLE20'){
+                    const discount = grandPrice * 0.20;
+                    const discountedPrice = grandPrice - discount;
+                    getGrandTotal.innerText = discountedPrice;
+                }
+                else{
+                    getGrandTotal.innerText = grandPrice;
+                }
+            })
+        }
+        else{
+            getGrandTotal.innerText = grandPrice;
+        }
+        //get the phone number field
+        // const getPhoneNum = document.getElementById('phone-num');
+        // console.log(getPhoneNum.value)
+        getPhoneNum()
+    }
+};
+
+//change the clicked seat background
+function changeSeatBG(id){
+    document.getElementById(id).style.backgroundColor ='#1DD100';
+    document.getElementById(id).style.color = 'white';
+};
+
+//total seat calculation
+function getTotalSeat(){
+    const totalSeat = document.getElementById('total-seat');
+    const leftSeat = totalSeat.innerText -1 ;
+    totalSeat.innerText = leftSeat;
+};
+
+//show the coupon input field
+function showCouponInput(){
+    const getCouponInput = document.getElementById('Coupon-input');
+    getCouponInput.classList.remove('hidden');
+};
+
+//show th next btn
+function showNextBtn(){
+    const getNextBtn = document.getElementById('next-btn');
+    getNextBtn.classList.remove('hidden');
+    
+};
+function actionNextBtn(){
+    hideSection();
+    showSeccessSection();
+}
+
+//get phone num input
+function getPhoneNum(){
+    const getPhoneNum = document.getElementById('phone-num');
+    if(getPhoneNum.value.length == 11){
+        showNextBtn()
+    }
+};
+//hide the main section
+function hideSection(){
+    const header = document.getElementById('header');
+    header.classList.add('hidden')
+    const mainSection = document.getElementById('main-section');
+    mainSection.classList.add('hidden');
+    const footer = document.getElementById('footer');
+    footer.classList.add('hidden');
+};
+//show the success section
+function showSeccessSection(){
+    const successSection = document.getElementById('success-section');
+    successSection.classList.remove('hidden');
+};
+
+function continueBtn(){
+    const header = document.getElementById('header');
+    header.classList.remove('hidden')
+    const mainSection = document.getElementById('main-section');
+    mainSection.classList.remove('hidden');
+    const footer = document.getElementById('footer');
+    footer.classList.remove('hidden');
+    const successSection = document.getElementById('success-section');
+    successSection.classList.add('hidden');
+}
---
+++ js/index.js
@@ -0,0 +1,116 @@
+const kbdElements = document.querySelectorAll('.kbd');
+const getInputField = document.getElementById('input-field');
+
+
+
+
+
+// Attach event listeners
+kbdElements.forEach((element) => {
+    element.onclick = handleClick;
+});
+
+function handleClick(event) {
+    const ticketPress = event.target.innerText;
+    const elementId = event.target.id;
+    // console.log(`Clicked on: ${elementId}`);
+    addTicketInfo(elementId);
+
+    //Only one time click the each button
+    if (event.target.classList.contains('clicked')) {
+        return;
+    }
+     // Already Clicked this button you can't click again
+     event.target.classList.add('clicked');
+
+    // total seat
+    if(ticketPress === elementId){
+        getTotalSeat();
+        
+        //get table body and set inner content
+        const getTableBody = document.getElementById('table-body');
+        // create a table
+        const tableTr = document.createElement('tr');
+        // create a table td
+        const seatNameTableTd = document.createElement('td');
+        seatNameTableTd.innerText = elementId;
+        tableTr.appendChild(seatNameTableTd);
+
+        const seatClassNameTd = document.createElement('td');
+        seatClassNameTd.innerText = 'Economy';
+        tableTr.appendChild(seatClassNameTd);
+        
+        const priceTableTd = document.createElement('td');
+        priceTableTd.innerText = 550;
+        tableTr.appendChild(priceTableTd);
+        // set the value in table body
+        getTableBody.appendChild(tableTr);
+
+        // get the total price
+        const getTotalPrice = document.getElementById('total-price');
+        const totalPriceInnerText = parseFloat(getTotalPrice.innerText);
+        const price = parseFloat(priceTableTd.innerText);
+        const totalTicketPrice = totalPriceInnerText + price;
+        getTotalPrice.innerText = totalTicketPrice;
+
+        const getGrandTotal = document.getElementById('grand-total');
+        // coupon section
+        if(getTableBody.childElementCount >= 4){
+            console.log('get your offer')
+            showCouponInput()
+            const grandPrice = parseFloat(getTotalPrice.innerText);
+            getGrandTotal.innerText = grandPrice;
+            console.log(getGrandTotal.innerText);
+            
+            // if click the coupon btn
+            const couponButton = document.getElementById('coupon-btn');
+            couponButton.addEventListener('click', function getCouponBtn() {
+            // trim use here for remove extra spaces
+            const couponCode = getInputField.value.trim();
+            if (couponCode === 'NEW15') {
+                const discount = grandPrice * 0.15;
+                const discountedPrice = grandPrice - discount;
+                getGrandTotal.innerText = discountedPrice.toFixed(2);
+                console.log(`Discounted Price: ${getGrandTotal.innerText}`);
+                }
+                else if(couponCode === 'COUPLE20'){
+                    const discount = grandPrice * 0.2;
+                    const discountedPrice = grandPrice - discount;
+                    getGrandTotal.innerText = discountedPrice.toFixed(2);
+                    console.log(`Discounted Price: ${getGrandTotal.innerText}`);
+                }
+                
+                else{
+                    const grandPrice = getTotalPrice.innerText;
+                    getGrandTotal.innerText = grandPrice;
+                }
+            }
+        )
+           
+        }
+        else{
+            const grandPrice = getTotalPrice.innerText;
+            getGrandTotal.innerText = grandPrice;
+            console.log(getGrandTotal.innerText);
+        }
+    }
+}
+
+function addTicketInfo(elementId){
+    document.getElementById(elementId).style.backgroundColor = '#1DD100';
+    document.getElementById(elementId).style.color= 'white'
+}
+
+function getTotalSeat(){
+    const totalSeat = document.getElementById('total-seat');
+    const leftSeat = totalSeat.innerText - 1;
+    totalSeat.innerText = leftSeat;
+}
+
+// show the coupon input section
+function showCouponInput(){
+    const getCouponInput = document.getElementById('Coupon-input');
+    getCouponInput.classList.remove('hidden');
+}
+
+
